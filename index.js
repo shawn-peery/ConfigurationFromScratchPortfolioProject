e2e7f8ec41f36abc34d047f458b643e902020c4a
Shawn Peery <shawn.w8465@gmail.com>
2024-03-20 20:58:10 -0600

    Using webpack for transpiling

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const moment = require("moment");
+import moment from "moment";
 
 console.log("Hello from JavaScript with Webpack!");
 console.log(moment().startOf("day").fromNow());
+
+const name = "Bob";
+const time = "today";
+
+console.log(`Hello ${name}, how are you ${time}`);
