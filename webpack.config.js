a2dd4b2512db8082248776e8790b3387705591d7
Shawn Peery <shawn.w8465@gmail.com>
2024-03-26 22:48:33 -0600

    Live reloading actually working now
    - Finally Live reloading is working for me

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,13 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
+  devtool: "inline-source-map",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
   },
   module: {
     rules: [
