cbcefde23641cd081bcc88b31e492f6560f8bca4
Shawn Peery <shawn.w8465@gmail.com>
2024-03-26 22:29:56 -0600

    Image Loader works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import "./style.scss";
 import _ from "lodash";
 
+import calendar from "../res/calendar.png";
+
 const component = () => {
   const element = document.createElement("div");
   element.classList.add("hello");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  // Add the image to our existing div.
+
+  const myIcon = new Image();
+
+  myIcon.src = calendar;
+
+  element.appendChild(myIcon);
+
   return element;
 };
 
